e14548a4f497390fd14208b7b531d27b00964573
clara <clara@clara.com>
2016-04-16 10:53:12 -0500

    Generating colors

--- src/com/company/ColorGUI.java
+++ src/com/company/ColorGUI.java
@@ -4,19 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class ColorGUI extends JFrame{
+public class ColorGUI extends JFrame {
 
-    int len = 1536;
+    int len = 1536;               //One pixel for each of the colors in the rainbow... actually just the ones a computer monitor can show (256 * 6)
     private JPanel colorPanel;
 
     ColorGUI() {
         ColorPanel panel = new ColorPanel();
         setContentPane(panel);
-        setPreferredSize(new Dimension(len, 320));
+        setPreferredSize(new Dimension(len, 120));
         pack();
         setVisible(true);
     }
-
 
     class ColorPanel extends JPanel {
 
@@ -24,67 +23,22 @@ public class ColorGUI extends JFrame{
         public void paintComponent(Graphics g) {
             super.paintComponents(g);
 
-            int maxColor = 16777216;
+            Rainbow rainbow = new Rainbow();
 
-            int ratio = maxColor / len;
+            //Select starting color
 
-            //scale maxColor into len and draw a rect that color.
-            for (int x = 0 ; x < len ; x ++ ){
-                java.awt.Color c = new java.awt.Color(x * ratio);  //try not to name things after Java library classes
-                g.setColor(c);
-                g.drawRect(x, 1, 1, 100);
+            rainbow.resetBlue();
+
+            //rainbow.resetRed();
+            //rainbow.resetCyan();
+            //rainbow.resetGreen();
+            //rainbow.resetMagenta();
+            //rainbow.resetYellow();
+
+            for (int x = 0; x < len; x++) {
+                g.setColor(rainbow.nextRainbowColor());
+                g.drawRect(x, 0, 1, 100);
             }
-
-            //Black through red
-
-            for (int x = 0 ; x < len ; x++ ) {
-                Color col = new Color((x%255), 0, 0);
-                g.setColor(col);
-                g.drawRect(x, 100, 1, 100);
-            }
-
-
-            //  Rainbow.
-
-            // R 255->0 G 0      B 255
-            // R 0      G 0->255 B 255
-            // R 0      G 255    B 255->0
-            // R 0->255 G 255    B 0
-            // R 255,   G 255->0 B 0
-            // R 255    G 0      B 0->255
-
-            int re = 255; int gr = 0; int bl = 255;
-
-            int[] c  = {re, gr, bl};
-
-            int diff = -1;
-
-            int xcord = 0;
-
-
-            for (int x = 0 ; x < 6 ; x++){
-
-                for (int d = 0 ; d < 255 ; d++){
-                    c[x % 3] += diff;
-                    System.out.println("x" + xcord + " " + c[0] + " " +  c[1] + " " + c[2]);
-                    java.awt.Color cl = new java.awt.Color(c[0], c[1], c[2]);
-                    g.setColor(cl);
-                    g.drawRect(xcord, 200, 1, 100);
-                    xcord++;
-                    if (xcord > len) { break; }
-                }
-
-                if (diff == 1) { diff = -1; }
-                else { diff = 1; }
-
-                if (xcord > len) { break; }
-            }
-
-
-
         }
-
-
     }
-
 }
---
+++ src/com/company/Rainbow.java
@@ -0,0 +1,127 @@
+package com.company;
+
+import java.awt.*;
+
+/**
+ * Created by clara on 4/16/16.
+ * To use: create a Rainbow object. Optionally call one of the resetRed, resetMagenta... methods
+ * to set start color, then call nextRainbowColor() as often as needed.
+ */
+
+public class Rainbow {
+
+    // Sequence of color changes to produce a rainbow.
+
+    // R 255->0 G 0      B 255        // Magenta to blue
+    // R 0      G 0->255 B 255        // Blue to cyan
+    // R 0      G 255    B 255->0     // Cyan to green
+    // R 0->255 G 255    B 0          // Green to yellow
+    // R 255    G 255->0 B 0          // Yellow to red
+    // R 255    G 0      B 0->255     // Red to magenta
+
+    // Some defaults - starts with Magenta turning into blue.
+    int r = 255;
+    int g = 0;
+    int b = 255;
+    int diff = -1;            // Change by 1
+
+    int numberOfDiffs = 0;    // Count how many times have modified a color. After 255 modifications, need to modify the next color in the cycle.
+    int rgbCycle = 0;         // 0 = modify red ; 1 = modify green ; 2 = modify blue
+
+    Rainbow() {
+        reset();
+    }
+
+    private void reset() {
+        numberOfDiffs = 0;
+    }
+
+    public void resetRed() {
+        reset();
+        r = 255;
+        g = 0;
+        b = 0;
+        diff = 1;
+        rgbCycle = 2;
+    }
+
+    public void resetBlue() {
+        reset();
+        r = 0;
+        g = 0;
+        b = 255;
+        diff = 1;
+        rgbCycle = 1;
+    }
+
+    public void resetGreen() {
+        reset();
+        r = 0;
+        g = 255;
+        b = 0;
+        diff = 1;
+        rgbCycle = 0;
+    }
+
+    public void resetYellow() {
+        reset();
+        r = 255;
+        g = 255;
+        b = 0;
+        diff = -1;
+        rgbCycle = 1;
+    }
+
+    public void resetMagenta() {
+        reset();
+        r = 255;
+        g = 0;
+        b = 255;
+        diff = -1;
+        rgbCycle = 0;
+    }
+
+    public void resetCyan() {
+        reset();
+        r = 0;
+        g = 255;
+        b = 255;
+        diff = -1;
+        rgbCycle = 2;
+    }
+
+
+    public Color nextRainbowColor() {
+
+        //Store RGB in array
+        int[] c = {r, g, b};
+
+        // to make use of array indexes to select color to modify
+        c[rgbCycle % 3] += diff;
+
+        //System.out.println(String.format("diff %d no_diffs %d cycle %d \t r %d g %d b %d", diff, numberOfDiffs, rgbCycle, r, g, b ));
+
+        Color color = new java.awt.Color(c[0], c[1], c[2]);
+
+        //Write out to global color variables
+        r = c[0];
+        g = c[1];
+        b = c[2];
+
+        //Track number of times the color has been modified
+        numberOfDiffs++;
+
+        //If have made 255 changes, then reset to 0 and start modifying the next color in the opposite way.
+        //Example: if have been increasing red, then need to decrease green next.
+
+        if (numberOfDiffs == 255) {
+            numberOfDiffs = 0;
+            rgbCycle = ( rgbCycle + 1 ) % 3;
+            diff = (diff == 1) ? -1 : 1;   // If diff is -1, set it to 1; if -1 set it to 1.
+        }
+
+        return color;
+    }
+
+}
+
